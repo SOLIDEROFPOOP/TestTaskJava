393ec6de4b952d07fc9035be2134f6664c49db94
Kapparov Murat <136073953+SOLIDEROFPOOP@users.noreply.github.com>
2024-03-02 03:10:04 +0600

    Project Init

---
+++ BankingServiceTask/src/main/java/com/example/bankingservicetask/BankingServiceTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankingservicetask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingServiceTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingServiceTaskApplication.class, args);
+    }
+
+}
---
+++ BankingServiceTask/src/test/java/com/example/bankingservicetask/BankingServiceTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankingservicetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingServiceTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
